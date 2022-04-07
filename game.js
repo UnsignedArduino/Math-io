c232646498d1960d0cc1cf6d9e643690e2e72a59
Ckyiu <38868705+UnsignedArduino@users.noreply.github.com>
2022-04-07 23:08:48 +0000

    Make core game class

---
+++ game.js
@@ -0,0 +1,13 @@
+class MathIO {
+  constructor() {
+    
+  }
+
+  update() {
+    
+  }
+
+  draw() {
+    
+  }
+}
--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const bg_color = 220;
 
 const window_diff = 20;
 
+let game = undefined;
+
 function setup() {
   createCanvas(windowWidth - window_diff, windowHeight - window_diff);
+  game = new MathIO();
 }
 
 function draw() {
   background(bg_color);
+  game.update();
+  game.draw();
 }
 
 function windowResized() {
